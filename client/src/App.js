6368df508bec07405c9bda59c8eb98cb90f3faa4
Mr. NFOR <gildasnfor@gmail.com>
2021-07-13 01:22:21 +0100

    signin and signup design

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import SetQuiz from "./pages/SetQuiz";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
+import Login from "./pages/auth/Login";
+import SignUp from "./pages/auth/SignUp";
 import SetQuestion from "./pages/SetQuestion";
 import QuizList from "./pages/QuizList";
 import Quizzes from "./pages/Quizzes";
@@ -13,6 +13,7 @@ import {
   Switch,
   Route,
   useHistory,
+  Redirect,
 } from "react-router-dom";
 import axiosInstance from "./axios/axios";
 import { connect } from "react-redux";
@@ -20,34 +21,36 @@ import { bindActionCreators } from "redux";
 import { setUser } from "./redux/actions/authActions";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import "./pages/auth/auth.css";
 
 function App({ setUser }) {
-  useEffect(() => {
-    axiosInstance
-      .get(`/auth/get-user`)
-      .then(res => {
-        setUser(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axiosInstance
+  //     .get(`/auth/get-user`)
+  //     .then(res => {
+  //       setUser(res.data);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }, []);
   return (
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Switch>
-            <Route path="/home" exact render={() => <Home />} />
+      <Router>
+        <Switch>
+          <Route path="/login" exact render={() => <Login />} />
+          <Route path="/signup" exact render={() => <SignUp />} />
+          <Redirect to="/login"></Redirect>
+          {/* <Route path="/home" exact render={() => <Home />} />
             <Route path="/" exact render={() => <Login />} />
             <Route path="/signup" exact render={() => <SignUp />} />
             <Route path="/set-quiz" exact render={() => <SetQuiz />} />
             <Route path="/set-question" exact render={() => <SetQuestion />} />
             <Route path="/update-quiz" exact render={() => <QuizList />} />
             <Route path="/select-quiz" exact render={() => <Quizzes />} />
-            <Route path="/take-quiz" exact render={() => <Quiz />} />
-          </Switch>
-        </Router>
-      </div>
+            <Route path="/take-quiz" exact render={() => <Quiz />} /> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
